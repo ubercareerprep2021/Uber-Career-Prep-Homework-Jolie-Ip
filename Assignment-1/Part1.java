d30dd43762741c9e914da94a4da7c37e6edc9b66
Jolie Ip <61493372+jolieip@users.noreply.github.com>
2021-03-19 01:15:33 -0700

    brute force solution for string permutation

--- Assignment-1/Part1.java
+++ Assignment-1/Part1.java
@@ -1,1 +1,20 @@
+/*
+Brute force solution
+1) convert both strings to character arrays
+2) compare the length of the strings through array length
+3) if equal, usee 2 for loops toiterate through the arrays and compare if they have the same elements
 
+Time Complexity = O(n^2)
+*/
+public boolean isStringPermutation(String s1, String s2) {
+    char[] str1 = s1.toCharArray();
+    char[] str2 = s2.toCharArray();
+    if (str1.length == str2.length) {
+      for (int i=0; i<str1.length; i++) {
+          for (int j = i+1; j<str1.length; j++) {
+              if (str1[i] == str2[j]) {return true;}
+          }
+      }
+    }
+  return false;
+}
