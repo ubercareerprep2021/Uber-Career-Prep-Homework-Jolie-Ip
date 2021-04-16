f5b13f8ef62df8a6f99d1f032349196f25f12170
Jolie Ip <61493372+jolieip@users.noreply.github.com>
2021-04-16 02:02:58 -0700

    Update Part3.java

--- Assignment-1/Part3.java
+++ Assignment-1/Part3.java
@@ -1,1 +1,34 @@
+public class thisStack {
+    int size;
+    int[] elements;
+    int index;
+  
+    public thisStack(int a) {
+        size = a;
+        elements = new int [size];
+        index = -1;
+    
+    }
 
+    public void push(int b){
+        index++;
+        elements[index] = b;
+    }
+  
+    public int pop(){
+        return elements[index--];
+        
+    }
+  
+    public int top(){
+        return elements[index];
+    }
+
+    public boolean isEmpty(){
+        if (index == -1) return true;
+        else return false;
+    }
+
+    public int size(){
+        return index + 1;
+    }
