bf3852c87a2e590be61eb39b0850d1725731535a
Jolie Ip <61493372+jolieip@users.noreply.github.com>
2021-04-16 02:31:38 -0700

    Update Part3.java

--- Assignment-1/Part3.java
+++ Assignment-1/Part3.java
@@ -32,3 +32,48 @@ public class thisStack {
     public int size(){
         return index + 1;
     }
+
+    
+    
+    // Queue class 
+public class thisQueue{
+    int[] numbers;
+    int size;
+    int count;
+    int start;
+    
+    public thisQueue(int a){
+        size = a;
+        numbers = new int[size];
+        count = -1;
+        start=0;
+    }
+    
+    public void enqueue(int b){
+        count++;
+        numbers[count]= b;
+    }
+    
+    public int dequeue(){
+        count--;
+        return numbers[start++];
+    }
+    public int rear(){
+        return numbers[count];
+    }
+    
+    public int front(){
+        return numbers[start];
+    }
+    
+    public int size(){
+        if (count > 0)
+        return count + 1;
+        else return 0;
+    }
+    public boolean isEmpty(){
+        if (count <= -1) return true;
+        else return false;
+    }
+
+    
