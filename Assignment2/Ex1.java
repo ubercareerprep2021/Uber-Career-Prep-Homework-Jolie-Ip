b1c8620a957a9b239e92b15844406ebc0394da73
Jolie Ip <61493372+jolieip@users.noreply.github.com>
2021-05-26 21:18:28 -0700

    Update Ex1.java

--- Assignment2/Ex1.java
+++ Assignment2/Ex1.java
@@ -1,13 +1,46 @@
-public static void print(TreeNode nodeA) {
+class Trees {
   
-  System.out.print(nodeA);
-    if (nodeA.left!= null) {
-      System.out.print(nodeA.left);
-      print(nodeA.left);
+    public static class Tree {
+      public TreeNode root;
+      public Tree(TreeNode root) {
+        this.root = root;
+      }
     }
   
-    if (nodeA.right!= null) {
-      System.out.print(nodeA.right);
-      print(nodeA.right);
+    public static class TreeNode {
+      public int data;
+      public TreeNode left = null;
+      public TreeNode right = null;
+      public TreeNode(int data, TreeNode left, TreeNode right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+      }
     }
-}
+  
+    public static void print(Tree nodeA) {
+
+        System.out.print(nodeA.data);
+
+        if (nodeA.left != null) {
+            print(nodeA.left);
+        }
+
+        if (nodeA.right != null) {
+            print(nodeA.right);
+        }
+        
+    }
+  
+    public static void main(String[] args) {
+      
+      TreeNode leftChild = new TreeNode(6, null, null);
+      TreeNode rightChild = new TreeNode(3, null, null);
+      TreeNode left = new TreeNode(7, null, null);
+      TreeNode right = new TreeNode(17, leftChild, rightChild);
+      TreeNode root = new TreeNode(1, left, right);
+      Tree tree = new Tree(root);
+      print(tree); 
+  
+    }
+  }
